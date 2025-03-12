5e5238572a4db1e659152adb031b54bf77eefe1e
Prabinthapadev21 <codinghub57@gmail.com>
2025-03-12 08:36:17 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("Hello World!");
+// variable decleration in javascript
+// we can decleare the variable using the let and var keyword
+// example
+var a = 10;
+let b = 20;
+// now days the var has become old and we usually use let keyword to decleare variables
+// so for displaying the output on the screen can be done by using the console.log keyword
+console.log("a="+a);
+console.log("b="+b);
+// now simply initializing the a string variable
+let string = "prabin";
+//simply displaying it 
+console.log("My name is "+string); 
